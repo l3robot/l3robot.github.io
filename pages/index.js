06d1a7afa444c5004163773c715d83d47a93efb6
Émile Robitaille <emile.courriel@gmail.com>
2025-03-01 12:31:38 -0500

    Improve looks of titles

--- pages/index.js
+++ pages/index.js
@@ -14,28 +14,19 @@ export default function Home() {
         </div>
         <div className={styles.contentWrapper}>
           <div className={styles.content}>
-            <h2>About Me</h2>
+            <h2 className={styles.sectionTitle}>About Me</h2>
             <p>I’m currently an Applied Scientist at Coveo, working in the Research and Development team.</p>
             <p>My passion lies in machine learning, especially its applications in decision support. I believe design is a key element in effectively conveying information.</p>
             <p>Also, a big fan of hockey and running!</p>
           </div>
           <div className={styles.skills}>
-            <h2>Skills</h2>
+            <h2 className={styles.sectionTitle}>Skills</h2>
             <ul>
               <li>Machine Learning</li>
               <li>Deep Learning</li>
               <li>LLM applications</li>
               <li>Software Development</li>
             </ul>
-          </div>
-        </div>
-        <div className={styles.blog}>
-          <h2>Insights & Research</h2>
-          <div className={styles.blogButtons}>
-            <a className={styles.blogButton} href="/blog/data-science">Data Science Exploration</a>
-            <a className={styles.blogButton} href="/blog/machine-learning">Machine Learning Innovations</a>
-            <a className={styles.blogButton} href="/blog/software-development">Software Dev Best Practices</a>
-            <a className={styles.blogButton} href="/blog/design">Design Principles</a>
           </div>
         </div>
       </div>
